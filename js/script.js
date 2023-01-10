b364a4f32983291b7aeaeb7868e56caff7d2d962
troxtele <105340164+troxtele@users.noreply.github.com>
2023-01-11 00:43:47 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 var swiper = new Swiper(".client_slider", {
-  slidesPerView: 2,
+  slidesPerView: 0,
   loop: true,
   pagination: {
     el: ".client_pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    1080: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
   },
 });
